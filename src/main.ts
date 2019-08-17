d8cf8892512eeb8d5836d950efdecbf1e071c798
Sem Bozhyk <sem.bozhyk@gmail.com>
2019-08-18 00:12:42 +0300

    Install tailwind

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+import 'tailwindcss/dist/base.css';
+import 'tailwindcss/dist/components.css';
+import 'tailwindcss/dist/utilities.css';
+
 Vue.config.productionTip = false;
 
 new Vue({
